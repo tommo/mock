31224ee9ed842eb24d35015f4ecfde57d38e3b73
Tommo Zhou <tommo.zhou@gmail.com>
2014-06-23 18:00:50 +0800

    [yaka] add slime spit

--- mock/core/MOAIHelpers.lua
+++ mock/core/MOAIHelpers.lua
@@ -141,14 +141,13 @@ LOG = MOAILogMgr.log
 function grabNextFrame( filepath, frameBuffer )
 	local image = MOAIImage.new()
 	frameBuffer = frameBuffer or MOAIGfxDevice.getFrameBuffer()
-	frameBuffer:grabNextFrame(image,
+	frameBuffer:grabNextFrame(
+		image,
 		function()
-			
-			local w,h=image:getSize()
-			local setRGBA=image.setRGBA
-			local getRGBA=image.getRGBA
-
-			-- io.write('postprocessing...')
+			local w,h = image:getSize()
+			local setRGBA = image.setRGBA
+			local getRGBA = image.getRGBA
+			io.write('postprocessing...')
 			for y=1,h do
 				for x=1,w do
 					local r,g,b,a=getRGBA(image,x,y)
@@ -156,7 +155,7 @@ function grabNextFrame( filepath, frameBuffer )
 				end
 			end
 			image:writePNG(filepath)
-			-- io.write('saved:   ',filepath,'\n')
+			io.write('saved:   ',filepath,'\n')
 		end)
 end
 
