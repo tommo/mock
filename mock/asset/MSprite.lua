d53f81cfe8074cc8806fdb685530018cb22deed6
Tommo Zhou <tommo.zhou@gmail.com>
2014-12-15 15:29:37 +0800

    [mock] add MSprite

---
+++ mock/asset/MSprite.lua
@@ -0,0 +1,146 @@
+module ('mock')
+
+local function MSpriteLoader( node )
+	--[[
+		sprite <package>
+			.moduletexture <texture>
+			.modules <uvquad[...]>
+			.frames  <deck_quadlist>
+	]]	
+	local data = loadAssetDataTable( node:getObjectFile('def') )
+	local textures = {}
+	local texRects = {}
+	local uvRects  = {}
+
+	--load images
+	local count = 0
+	for id, name in pairs( data.atlases ) do
+		count = count+1
+		if count>1 then error("multiple image not supported") end
+
+		local imgFile = node:getSiblingPath( name )
+		local tex, texNode = loadAsset( imgFile )
+		if not tex then 
+			_error( 'cannot load sprite texture', imgFile )
+			return nil
+		end
+		local mtex, uvRect = tex:getMoaiTextureUV() 
+		textures[ count ] = mtex
+		local tw, th = tex:getSize()
+		local ox, oy = 0, 0 --todo
+		texRects[ id ] = { tw, th, ox, oy } 
+	end
+
+	local deck = MOAIGfxQuadListDeck2D.new()
+	--count parts
+	local partCount = 0
+	for frameId, frame in pairs( data.frames ) do
+		partCount = partCount + #frame.parts
+	end
+
+	local moduleCount = table.len( data.modules )
+	local frameCount = table.len( data.frames )
+	--
+	deck:reservePairs( partCount ) --one pair per frame component
+	deck:reserveQuads( partCount ) --one quad per frame component
+	deck:reserveUVQuads( moduleCount ) --one uv per module
+	deck:reserveLists( frameCount ) --one list per frame
+
+	deck:setTexture( textures[1] )
+
+	local moduleIdToIndex = {}
+	local frameIdToIndex  = {}
+	local i = 0
+	for id, m in pairs( data.modules ) do
+		i = i + 1
+		moduleIdToIndex[ id ] = i
+		local x, y, w, h = unpack( m.rect )
+		local texRect = texRects[ m.atlas ]
+		local tw, th, ox, oy = unpack( texRect )
+		local u0, v0, u1, v1 = (x+ox+0.1)/tw, (y+oy+0.1)/th, (x+ox+w)/tw, (y+oy+h)/th
+		m.uv = {u0, v0, u1, v1}
+		m.index = i
+		deck:setUVRect( i, u0, v0, u1, v1 )
+	end
+
+	local partIdx = 1
+	local i = 0
+	for id, frame in pairs( data.frames ) do
+		i = i + 1
+		frameIdToIndex[ id ] = i
+		local basePartId = partIdx
+		frame.index = i
+		for j, part in ipairs( frame.parts ) do
+			local m = data.modules[ part[1] ]
+			local ox, oy = part[2], part[3]
+			local _, _, w, h = unpack( m.rect )
+			local x0, y0 = ox, oy
+			local x1, y1 = x0 + w, y0 + h
+			-- deck:setRect( partIdx, x0 + ox, y0 + oy, x1 + ox, y1 + oy )
+			deck:setRect( partIdx, x0, -y0, x1, -y1 )
+			deck:setPair( partIdx, m.index, partIdx )
+			partIdx = partIdx + 1
+		end
+		deck:setList( i, basePartId, partIdx - basePartId )
+	end
+	preloadIntoAssetNode( node:getChildPath('frames'), deck )
+
+	--animations
+	local EaseFlat   = MOAIEaseType.FLAT
+	local EaseLinear = MOAIEaseType.LINEAR
+	local animations = {}
+	for id, animation in pairs( data.anims ) do
+		local name = animation.name
+		local sequence = animation.seq
+		--create anim curve
+		local indexCurve   = MOAIAnimCurve.new()
+		local offsetXCurve = MOAIAnimCurve.new()
+		local offsetYCurve = MOAIAnimCurve.new()
+		local count = #sequence
+
+		indexCurve   : reserveKeys( count + 1 )
+		offsetXCurve : reserveKeys( count + 1 )
+		offsetYCurve : reserveKeys( count + 1 )
+
+		--TODO: support flags? or just forbid it!!!!
+		local offsetEaseType = EaseLinear
+		local ftime = 0
+		for fid, f in ipairs( sequence ) do
+
+			local frameId, delay, ox, oy = unpack( f )
+			local frame = data.frames[ frameId ]
+			offsetXCurve:setKey( fid, ftime, ox, offsetEaseType )
+			offsetYCurve:setKey( fid, ftime, -oy, offsetEaseType )
+			indexCurve  :setKey( fid, ftime, frame.index, EaseFlat )
+
+			delay = math.max( delay, 1 )
+			ftime = ftime + delay  --will use anim:setSpeed to fit real playback FPS
+
+			if fid == count then --copy last frame to make loop smooth
+				offsetXCurve:setKey( fid + 1, ftime, ox, offsetEaseType )
+				offsetYCurve:setKey( fid + 1, ftime, -oy, offsetEaseType )
+				indexCurve  :setKey( fid + 1, ftime, frame.index, EaseFlat )
+			end
+
+		end
+
+		animations[ name ] = {
+			offsetXCurve = offsetXCurve,
+			offsetYCurve = offsetYCurve,
+			indexCurve   = indexCurve,
+			length       = ftime,
+			name         = name,
+		}
+	end
+
+	local sprite = {
+		frameDeck  = deck,
+		animations = animations,
+		texture    = tex
+	}
+
+	return sprite
+end
+
+
+registerAssetLoader( 'msprite', MSpriteLoader )
---
+++ mock/component/MSprite.lua
@@ -0,0 +1,241 @@
+--[[
+* MOCK framework for Moai
+
+* Copyright (C) 2012 Tommo Zhou(tommo.zhou@gmail.com).  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be
+* included in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+]]
+module 'mock'
+
+--------------------------------------------------------------------
+CLASS: MSprite ( RenderComponent )
+	:MODEL {
+		Field 'sprite' :asset( 'msprite' ) :getset('Sprite');
+		Field 'default' :string() :selection( 'getClipNames' );
+		Field 'autoPlay' :boolean();
+	}
+
+wrapWithMoaiPropMethods( MSprite, 'prop' )
+
+mock.registerComponent( 'MSprite', MSprite )
+mock.registerEntityWithComponent( 'MSprite', MSprite )
+--------------------------------------------------------------------
+
+function MSprite:__init()
+	self.prop        = MOAIProp.new()
+	self.driver      = MOAIAnim.new()
+	self.spriteData = false
+	self.currentClip = false
+	self.playFPS     = 60
+	self.playSpeed   = 1
+	self.driver:reserveLinks( 3 ) --offset x & offset y & frame index	
+end
+
+function MSprite:onAttach( entity )
+	return entity:_attachProp( self.prop )
+end
+
+function MSprite:onDetach( entity )
+	self:stop()
+	return entity:_detachProp( self.prop )
+end
+
+function MSprite:onStart( entity )
+	if self.autoPlay and self.default then
+		self:play( self.default )
+	end
+end
+
+function MSprite:setSprite( path )
+	self:stop( true )
+	self.spritePath = path 
+
+	local spriteData, node = loadAsset( path )
+	--TODO? assert asset node type
+	if spriteData then
+		self:stop( true )
+		self.currentClip = false
+		self.spriteData = spriteData
+		self.prop:setDeck( spriteData.frameDeck )
+		self.prop:setIndex( 0 )
+		self.prop:forceUpdate()
+	end
+
+end
+
+function MSprite:getSprite()
+	return self.spritePath
+end
+
+function MSprite:getSpriteData()
+	return self.spriteData
+end
+
+function MSprite:getClipNames()
+	local data = self.spriteData
+	if not data then return nil end
+	local result = {}
+	for k,i in pairs( data.animations ) do
+		table.insert( result, { k, k } )
+	end
+	return result
+end
+
+function MSprite:setScissorRect( r )
+	return self.prop:setScissorRect( r )
+end
+
+function MSprite:getClipTable()
+	local data = self.spriteData
+	if not data then 
+		_error('animation not load', 2)
+		return nil
+	end
+	return data.animations
+end
+
+function MSprite:getClip( name )
+	local data = self.spriteData
+	if not data then 
+		_error('animation not load', 2)
+		return nil
+	end
+	return data.animations[ name ]
+end
+
+function MSprite:getClipLength( name )
+	local clip = name and self:getClip( name ) or self.currentClip
+	if clip then return clip.length * self.playFPS end
+end
+
+function MSprite:setClip( name, mode )
+	if self.currentClip and self.currentClip.name == name then return false end
+
+	local clip = self:getClip( name )
+	if not clip then 
+		_error( 'animation clip not found:'..name )
+		return
+	end
+	self.currentClip=clip
+	if self.driver then self.driver:stop() end
+	---bind animcurve to driver
+	local driver       = MOAIAnim.new()
+	local indexCurve   = clip.indexCurve
+	local offsetXCurve = clip.offsetXCurve
+	local offsetYCurve = clip.offsetYCurve
+
+	driver:reserveLinks( 3 )
+	driver:setLink( 1, indexCurve,   self.prop, MOAIProp.ATTR_INDEX )
+	-- driver:setLink( 2, offsetXCurve, self.prop, MOAIProp.ATTR_X_LOC )
+	-- driver:setLink( 3, offsetYCurve, self.prop, MOAIProp.ATTR_Y_LOC )
+	driver:setMode( mode or clip.mode or MOAITimer.NORMAL )
+
+	self.driver = driver
+
+	self:setFPS(self.playFPS)
+	self:apply( 0 )
+	self:setTime( 0 )
+
+	return true
+end
+
+-----------
+function MSprite:setFPS( fps )
+	self.playFPS = fps
+	self:setSpeed( self.playSpeed )
+end
+
+function MSprite:getFPS()
+	return self.playFPS
+end
+
+function MSprite:setSpeed( speed )
+	speed = speed or 1
+	self.playSpeed = speed
+	self.driver:setSpeed( speed * self.playFPS )
+end
+
+function MSprite:getSpeed()
+	return self.playSpeed
+end
+
+function MSprite:setTime( time )
+	return self.driver:setTime( time )
+end
+
+function MSprite:apply( time )
+	return self.driver:apply( time / self.playFPS )
+end
+
+-----------Play control
+function MSprite:play( clipName, mode )
+	if self:setClip( clipName, mode ) then return self:start() end
+end
+
+function MSprite:resetAndPlay( clipName, mode )
+	if self:setClip( clipName, mode ) then  --playing a new clip
+		return self:start()
+	else --same as playing clip
+		self:setTime( 0 )
+		self:apply( 0 )
+		self:start()
+	end
+end
+
+function MSprite:start()
+	self.driver:start()
+end
+
+function MSprite:reset()
+	self:setTime( 0 )
+end
+
+function MSprite:stop( reset )
+	self.driver:stop()
+	if reset then return self:reset() end
+end
+
+function MSprite:pause( paused )
+	self.driver:pause( paused )
+end
+
+function MSprite:isPaused()
+end
+
+function MSprite:wait()
+	return MOAICoroutine.blockOnAction( self.driver )
+end
+
+function MSprite:isPlaying()
+	return self.driver:isBusy()
+end
+
+function MSprite:setBlend( b )
+	self.blend = b	
+	setPropBlend( self.prop, b )
+end
+
+function MSprite:setVisible( f )
+	return self.prop:setVisible( f )
+end
+
+function MSprite:isVisible()
+	return self.prop:isVisible()
+end
