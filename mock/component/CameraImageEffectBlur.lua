fbf65d45284a79d8353f0b264fd1418a0d1c477a
Tommo Zhou <tommo.zhou@gmail.com>
2015-06-12 17:23:33 +0800

    more gizmos

---
+++ mock/component/CameraImageEffectBlur.lua
@@ -0,0 +1,80 @@
+module 'mock'
+
+local function buildBlurShader()
+	local vsh = [[
+		attribute vec4 position;
+		attribute vec2 uv;
+		attribute vec4 color;
+
+		varying LOWP vec4 colorVarying;
+		varying MEDP vec2 uvVarying;
+
+		void main () {
+			gl_Position = position;
+			uvVarying = uv;
+			colorVarying = color;
+		}
+	]]
+
+	local fsh = [[	
+		varying LOWP vec4 colorVarying;
+		varying MEDP vec2 uvVarying;
+
+		uniform sampler2D sampler;
+		uniform float viewWidth;
+		uniform float viewHeight;
+
+		void main () {
+			float du = 1.0/viewWidth  * 2.0;
+			float dv = 1.0/viewHeight * 2.0;
+			LOWP vec4 color = texture2D ( sampler, uvVarying );
+			color += texture2D ( sampler, uvVarying + vec2( 0.0, dv )  );
+			color += texture2D ( sampler, uvVarying + vec2( 0.0, -dv ) );
+			color += texture2D ( sampler, uvVarying + vec2( du, 0.0 )  );
+			color += texture2D ( sampler, uvVarying + vec2( -du, 0.0 ) );
+			gl_FragColor = color / 5.0;
+		}
+	]]
+
+	local prog = buildShaderProgramFromString( vsh, fsh, {
+		uniforms = {
+			{
+				name = "sampler",
+				type = "sampler",
+				value = 1
+			},
+			{
+				name = "time",
+				type = "float",
+				value = 0
+			},
+			{
+				name = "intensity",
+				type = "float",
+				value = 2
+			}
+		},
+		globals = {
+			{
+				name = 'viewWidth',
+				type = 'GLOBAL_VIEW_WIDTH',
+			},
+			{
+				name = 'viewHeight',
+				type = 'GLOBAL_VIEW_HEIGHT',
+			}
+		}
+	} )
+	return prog:buildShader():getMoaiShader()
+end
+
+--------------------------------------------------------------------
+CLASS: CameraImageEffectBlur ( CameraImageEffect )
+	:MODEL{}
+
+function CameraImageEffectBlur:onBuild( prop, layer )
+	prop:setShader( assert( buildBlurShader() ) )
+end
+
+
+mock.registerComponent( 'CameraImageEffectBlur', CameraImageEffectBlur )
