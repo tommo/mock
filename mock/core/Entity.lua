feadd9291d2207d16bd2512322b120e69226aded
tommo <tommo.zhou@gmail.com>
2016-08-06 10:47:03 +0800

    remove entitygroup when reparent to entity

--- mock/core/Entity.lua
+++ mock/core/Entity.lua
@@ -694,7 +694,9 @@ function Entity:reparentGroup( group )
 	if self._entityGroup then
 		self._entityGroup:removeEntity( self )
 	end
-	group:addEntity( self )
+	if group then
+		group:addEntity( self )
+	end
 end
 
 function Entity:reparent( entity )
@@ -707,6 +709,7 @@ function Entity:reparent( entity )
 	end
 	self.parent = entity
 	if entity then
+		self:reparentGroup( nil )
 		entity.children[ self ] = true
 		entity:_attachChildEntity( self )
 	end
