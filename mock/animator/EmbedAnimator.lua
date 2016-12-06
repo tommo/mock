581d8cc2ad344a56694847a71e932e964befd254
tommo <tommo.zhou@gmail.com>
2016-12-06 15:26:42 +0800

    add EmbedAnimator data export support

--- mock/animator/EmbedAnimator.lua
+++ mock/animator/EmbedAnimator.lua
@@ -10,11 +10,19 @@ local function loadEmbdedAnimatorData( strData )
 end
 
 --------------------------------------------------------------------
+CLASS: ExportAnimatorParam ()
+:MODEL {
+	Field 'animatorData'	 :asset( 'animator_data' );
+}
+
+--------------------------------------------------------------------
 CLASS: EmbedAnimator ( Animator )
 	:MODEL{
 		Field 'serializedData' :string() :no_edit() :getset( 'SerializedData' );
 		Field 'data' :asset('animator_data')  :no_edit() :no_save();
 		Field 'uniqueKey' :string() :no_edit();
+		'----';
+		Field 'exportData' :action( 'toolActionExportData' );
 	}
 
 registerComponent( 'EmbedAnimator', EmbedAnimator )
@@ -40,4 +48,17 @@ end
 function EmbedAnimator:setSerializedData( strData )
 	local animatorData = loadEmbdedAnimatorData( strData )
 	self.data = animatorData
+end
+
+function EmbedAnimator:toolActionExportData()
+	local param = ExportAnimatorParam()
+	if mock_edit.requestProperty( 'exporting animator data', param ) then
+		if not param.animatorData then
+			mock_edit.alertMessage( 'message', 'no animator data specified', 'info' )
+			return false
+		end
+		local node = getAssetNode( param.animatorData )
+		mock.serializeToFile( self.data, node:getObjectFile( 'data' )  )
+		mock_edit.alertMessage( 'message', 'animator data exported!', 'info' )
+	end
 end
