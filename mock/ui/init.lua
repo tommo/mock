7edb4c86519031174843cb2576481432596f223b
tommo <tommo.zhou@gmail.com>
2016-12-12 11:29:26 +0800

    renamed TB module

--- mock/ui/init.lua
+++ mock/ui/init.lua
@@ -1,30 +1,3 @@
 module 'mock'
 
---------------------------------------------------------------------
-require 'mock.ui.TBManager'
-require 'mock.ui.TBSkin'
-require 'mock.ui.TBScheme'
-
-require 'mock.ui.TBCanvas'
-
---------------------------------------------------------------------
-require 'mock.ui.TBWidget'
-
-require 'mock.ui.TBWindow'
-require 'mock.ui.TBContainer'
-require 'mock.ui.TBLayout'
-require 'mock.ui.TBSchemeContainer'
--- require 'mock.ui.TBTabLayout'
--- require 'mock.ui.TBContainer'
--- require 'mock.ui.TBScrollContainer'
--- require 'mock.ui.TBSection'
-
-require 'mock.ui.TBButton'
-require 'mock.ui.TBTextField'
--- require 'mock.ui.TBRadioButton'
--- -- require 'mock.ui.TBEditField'
-
-
-
---------------------------------------------------------------------
-require 'mock.ui.ButtonMessageSender'
+require 'mock.ui.tb'
---
+++ mock/ui/tb/init.lua
@@ -0,0 +1,30 @@
+module 'mock'
+
+--------------------------------------------------------------------
+require 'mock.ui.tb.TBManager'
+require 'mock.ui.tb.TBSkin'
+require 'mock.ui.tb.TBScheme'
+
+require 'mock.ui.tb.TBCanvas'
+
+--------------------------------------------------------------------
+require 'mock.ui.tb.TBWidget'
+
+require 'mock.ui.tb.TBWindow'
+require 'mock.ui.tb.TBContainer'
+require 'mock.ui.tb.TBLayout'
+require 'mock.ui.tb.TBSchemeContainer'
+-- require 'mock.ui.tb.TBTabLayout'
+-- require 'mock.ui.tb.TBContainer'
+-- require 'mock.ui.tb.TBScrollContainer'
+-- require 'mock.ui.tb.TBSection'
+
+require 'mock.ui.tb.TBButton'
+require 'mock.ui.tb.TBTextField'
+-- require 'mock.ui.tb.TBRadioButton'
+-- -- require 'mock.ui.tb.TBEditField'
+
+
+
+--------------------------------------------------------------------
+require 'mock.ui.tb.ButtonMessageSender'
